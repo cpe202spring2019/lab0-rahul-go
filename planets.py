f7de976f6c0b5632775348d7439e79241fc0a961
rahul-go <rahulxgoyal@gmail.com>
2019-04-03 12:56:26 -0700

    Added loop

--- planets.py
+++ planets.py
@@ -1,12 +1,14 @@
 def weight_on_planets():
-    try:
-        # Weight on earth (pounds)
-        weight = int(input("What do you weigh on earth? "))
-    except ValueError:
-        print("Invalid input!")
-    else:
-        print("\nOn Mars you would weigh", weight * 0.38, "pounds."             # Weight on Mars (pounds)
-              "\nOn Jupiter you would weigh", weight * 2.34, "pounds.")         # Weight on Jupiter (pounds)
+	while(True):
+	    try:
+	        # Weight on earth (pounds)
+	        weight = int(input("What do you weigh on earth? "))
+	    except ValueError:
+	        print("Invalid input, try again!")
+	    else:
+	        print("\nOn Mars you would weigh", weight * 0.38, "pounds."             # Weight on Mars (pounds)
+	              "\nOn Jupiter you would weigh", weight * 2.34, "pounds.")         # Weight on Jupiter (pounds)
+	        break
 
 
 
